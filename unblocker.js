6f6c308fcfc7a45d3948384a3e9b253047c0f8e4
skylord17110 <129102298+skylord17110@users.noreply.github.com>
2024-02-24 11:41:13 -0500

    Update unblocker.js

--- unblocker.js
+++ unblocker.js
@@ -1,18 +1,1 @@
-javascript:(function() {
-    // Remove Securly elements
-    const elementsToRemove = document.querySelectorAll("div.head-top, div.wonderbar");
-    elementsToRemove.forEach(function(element) {
-        element.remove();
-    });
-
-    // Remove certain buttons
-    const buttonsToRemove = document.querySelectorAll("button.slick-prev.slick-arrow.slick-disabled, button.slick-next.slick-arrow.slick, button.slick-prev.slick-arrow, button.slick-next.slick-arrow.slick-disabled");
-    buttonsToRemove.forEach(function(button) {
-        button.remove();
-    });
-
-    // Optionally, you can add more code here to remove other elements or perform additional actions
-
-    // Alert the user that Securly has been disabled
-    alert("Securly Successfully Disabled!");
-})();
+javascript:document.cookie.includes("securly_disabled=true")?(document.cookie="securly_disabled=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;",alert("Securly is now enabled.")):(document.cookie="securly_disabled=true; path=/;",alert("Securly is now disabled."));
