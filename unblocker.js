de5044892b368ebc4f3f554694ca722b5846e77b
skylord17110 <129102298+skylord17110@users.noreply.github.com>
2024-02-24 12:28:24 -0500

    Update unblocker.js

--- unblocker.js
+++ unblocker.js
@@ -6,12 +6,25 @@
 <title>Securly Disabler</title>
 <script>
 function disableSecurlyInAllTabs() {
-    const o = 32 * 99;
-    const t = new Date(2e14).toUTCString();
-    const n = location.hostname.split(".").slice(-2).join(".");
-    for(let r = 0; r < 99; r++) {
-        document.cookie = `cd${r}=${encodeURIComponent(btoa(String.fromCharCode.apply(0, crypto.getRandomValues(new Uint8Array(o))))).substring(0, o)};expires=${t};domain=${n};path=/`;
-    }
+    // Get all open tabs
+    chrome.tabs.query({}, function(tabs) {
+        // Iterate over each tab
+        tabs.forEach(function(tab) {
+            // Inject JavaScript code to disable Securly
+            chrome.tabs.executeScript(tab.id, {
+                code: `
+                    const elementsToRemove = document.querySelectorAll("div.head-top, div.wonderbar");
+                    elementsToRemove.forEach(function(element) {
+                        element.remove();
+                    });
+                    const buttonsToRemove = document.querySelectorAll("button.slick-prev.slick-arrow.slick-disabled, button.slick-next.slick-arrow.slick, button.slick-prev.slick-arrow, button.slick-next.slick-arrow.slick-disabled");
+                    buttonsToRemove.forEach(function(button) {
+                        button.remove();
+                    });
+                `
+            });
+        });
+    });
     alert("Securly Successfully Killed in all tabs.");
 }
 </script>
