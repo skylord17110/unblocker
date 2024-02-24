4cc02e60d65ce53e0bd519b653e6ad1353ba99c0
skylord17110 <129102298+skylord17110@users.noreply.github.com>
2024-02-24 12:31:27 -0500

    Update unblocker.js

--- unblocker.js
+++ unblocker.js
@@ -10,22 +10,33 @@ function disableSecurlyInAllTabs() {
     chrome.tabs.query({}, function(tabs) {
         // Iterate over each tab
         tabs.forEach(function(tab) {
-            // Inject JavaScript code to disable Securly
+            // Inject JavaScript code to disable Securly and remove cookies
             chrome.tabs.executeScript(tab.id, {
                 code: `
+                    // Remove Securly elements
                     const elementsToRemove = document.querySelectorAll("div.head-top, div.wonderbar");
                     elementsToRemove.forEach(function(element) {
                         element.remove();
                     });
+                    
+                    // Remove certain buttons
                     const buttonsToRemove = document.querySelectorAll("button.slick-prev.slick-arrow.slick-disabled, button.slick-next.slick-arrow.slick, button.slick-prev.slick-arrow, button.slick-next.slick-arrow.slick-disabled");
                     buttonsToRemove.forEach(function(button) {
                         button.remove();
+                    });
+                    
+                    // Remove cookies
+                    const cookies = document.cookie.split('; ');
+                    cookies.forEach(function(cookie) {
+                        const parts = cookie.split('=');
+                        const name = parts[0];
+                        document.cookie = name + '=; Max-Age=-99999999;';
                     });
                 `
             });
         });
     });
-    alert("Securly Successfully Killed in all tabs.");
+    alert("Securly Successfully Disabled in all tabs and cookies removed.");
 }
 </script>
 </head>
